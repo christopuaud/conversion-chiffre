5b25f22fe1b77a5daf5e919c1ea1f620dbf81d67
christopuaud <puaud.christopher@outlook.fr>
2023-01-25 13:31:45 +0100

    test

--- src/NombreRomain_I1.java
+++ src/NombreRomain_I1.java
@@ -6,27 +6,47 @@ public class NombreRomain_I1 {
         int rest = nombreArabe;
         String res = "";
         do {
-            if (rest >= 10) {
-                res += "X";
-                rest -= 10;
+            if (rest >= 100) {
+                res += "C";
+                rest -= 100;
             } else {
-                if (rest == 9) {
-                    res += "IX";
-                    rest -= 9;
+                if (rest >= 90) {
+                    res += "XC";
+                    rest -= 90;
                 } else {
-                    if (rest >= 5) {
-                        res += "V";
-                        rest -= 5;
+                    if (rest >= 50) {
+                        res += "L";
+                        rest -= 50;
                     } else {
-                        if (rest % 5 == 4) {
-                            res += "IV";
-                            rest -= 4;
+                        if (rest >= 40) {
+                            res += "XL";
+                            rest -= 40;
                         } else {
-                            res += "I".repeat(rest);
-                            rest -= rest;
+                            if (rest >= 10) {
+                                res += "X";
+                                rest -= 10;
+                            } else {
+                                if (rest == 9) {
+                                    res += "IX";
+                                    rest -= 9;
+                                } else {
+                                    if (rest >= 5) {
+                                        res += "V";
+                                        rest -= 5;
+                                    } else {
+                                        if (rest % 5 == 4) {
+                                            res += "IV";
+                                            rest -= 4;
+                                        } else {
+                                            res += "I".repeat(rest);
+                                            rest -= rest;
+                                        }
+                                    }
+
+                                }
+                            }
                         }
                     }
-
                 }
             }
         } while (rest != 0 | rest < 0);
--- test/NombreRomainTest.java
+++ test/NombreRomainTest.java
@@ -113,5 +113,153 @@ public class NombreRomainTest {
                  assertEquals("X", nombreRomain);
                  
              }
-
+             @Test
+             public void Test11() {
+                  // ETANT DONNE le chiffre 11
+                  int chiffreArabe =11;
+                  //QUAND on le convertir en nombre romain
+                  String nombreRomain = NombreRomain_I1.Convertir(chiffreArabe);
+          
+                  assertEquals("XI", nombreRomain);
+                  
+              }
+              @Test
+              public void Test12() {
+                   // ETANT DONNE le chiffre 12
+                   int chiffreArabe =12;
+                   //QUAND on le convertir en nombre romain
+                   String nombreRomain = NombreRomain_I1.Convertir(chiffreArabe);
+           
+                   assertEquals("XII", nombreRomain);
+                   
+               }
+               @Test
+               public void Test13() {
+                    // ETANT DONNE le chiffre 13
+                    int chiffreArabe =13;
+                    //QUAND on le convertir en nombre romain
+                    String nombreRomain = NombreRomain_I1.Convertir(chiffreArabe);
+            
+                    assertEquals("XIII", nombreRomain);
+                    
+                }
+                @Test
+                public void Test14() {
+                     // ETANT DONNE le chiffre 14
+                     int chiffreArabe =14;
+                     //QUAND on le convertir en nombre romain
+                     String nombreRomain = NombreRomain_I1.Convertir(chiffreArabe);
+             
+                     assertEquals("XIV", nombreRomain);
+                     
+                 }
+                 @Test
+                 public void Test15() {
+                      // ETANT DONNE le chiffre 15
+                      int chiffreArabe =15;
+                      //QUAND on le convertir en nombre romain
+                      String nombreRomain = NombreRomain_I1.Convertir(chiffreArabe);
+              
+                      assertEquals("XV", nombreRomain);
+                      
+                  }
+                  @Test
+                  public void Test16() {
+                       // ETANT DONNE le chiffre 16
+                       int chiffreArabe =16;
+                       //QUAND on le convertir en nombre romain
+                       String nombreRomain = NombreRomain_I1.Convertir(chiffreArabe);
+               
+                       assertEquals("XVI", nombreRomain);
+                       
+                   }
+                   @Test
+                   public void Test17() {
+                        // ETANT DONNE le chiffre 17
+                        int chiffreArabe =17;
+                        //QUAND on le convertir en nombre romain
+                        String nombreRomain = NombreRomain_I1.Convertir(chiffreArabe);
+                
+                        assertEquals("XVII", nombreRomain);
+                        
+                    }
+                    @Test
+                    public void Test18() {
+                         // ETANT DONNE le chiffre 8
+                         int chiffreArabe =18;
+                         //QUAND on le convertir en nombre romain
+                         String nombreRomain = NombreRomain_I1.Convertir(chiffreArabe);
+                 
+                         assertEquals("XVIII", nombreRomain);
+                         
+                     }
+                     @Test
+                     public void Test19() {
+                          // ETANT DONNE le chiffre 19
+                          int chiffreArabe =19;
+                          //QUAND on le convertir en nombre romain
+                          String nombreRomain = NombreRomain_I1.Convertir(chiffreArabe);
+                  
+                          assertEquals("XIX", nombreRomain);
+                          
+                      }
+                      @Test
+                      public void Test20() {
+                           // ETANT DONNE le chiffre 20
+                           int chiffreArabe =20;
+                           //QUAND on le convertir en nombre romain
+                           String nombreRomain = NombreRomain_I1.Convertir(chiffreArabe);
+                   
+                           assertEquals("XX", nombreRomain);
+                           
+                       }
+                       @Test
+                       public void Test39() {
+                            // ETANT DONNE le chiffre 39
+                            int chiffreArabe =39;
+                            //QUAND on le convertir en nombre romain
+                            String nombreRomain = NombreRomain_I1.Convertir(chiffreArabe);
+                    
+                            assertEquals("XXXIX", nombreRomain);
+                            
+                        }
+                        @Test
+                       public void Test49() {
+                            // ETANT DONNE le chiffre 49
+                            int chiffreArabe =49;
+                            //QUAND on le convertir en nombre romain
+                            String nombreRomain = NombreRomain_I1.Convertir(chiffreArabe);
+                    
+                            assertEquals("XLIX", nombreRomain);
+                            
+                        }
+                        @Test
+                        public void Test99() {
+                            // ETANT DONNE le chiffre 99
+                            int chiffreArabe =99;
+                            //QUAND on le convertir en nombre romain
+                            String nombreRomain = NombreRomain_I1.Convertir(chiffreArabe);
+                    
+                            assertEquals("XCIX", nombreRomain);
+                            
+                        }
+                        @Test
+                        public void Test200() {
+                            // ETANT DONNE le chiffre 200
+                            int chiffreArabe =200;
+                            //QUAND on le convertir en nombre romain
+                            String nombreRomain = NombreRomain_I1.Convertir(chiffreArabe);
+                    
+                            assertEquals("CC", nombreRomain);
+                            
+                        }
+                        @Test
+                        public void Test250() {
+                            // ETANT DONNE le chiffre 250
+                            int chiffreArabe =250;
+                            //QUAND on le convertir en nombre romain
+                            String nombreRomain = NombreRomain_I1.Convertir(chiffreArabe);
+                            assertEquals("CCL", nombreRomain);
+                            
+                        }
 }
